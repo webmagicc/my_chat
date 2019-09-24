1f96e54d4a4864cde2d30de3f4f18a9e6241f04c
Vladimir <vladimir@enkonix.com>
2019-09-24 08:01:55 +0300

    add admin part

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 
+from .models import Online, GroupParticipant, ChatMessage, ChatSession, ChatGroup
+
+
+@admin.register(Online)
+class OnlineAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(GroupParticipant)
+class GroupParticipantAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ChatSession)
+class ChatSessionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ChatGroup)
+class ChatGroupAdmin(admin.ModelAdmin):
+    pass
+
 
 
 
